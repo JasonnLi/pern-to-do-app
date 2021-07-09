3349e8ff9549d0f51f0262cf93fa3019407505ed
JasonnLi <jasonlee5233@gmail.com>
2021-07-09 17:23:50 +1000

    remove env from git

--- client/infra/index.ts
+++ client/infra/index.ts
@@ -3,21 +3,21 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
+// export const bucketName = bucket.id;
 
-// const baseTags = {
-//     Project: "Infra",
-//     PulumiStack: pulumi.getStack()
-// };
+const baseTags = {
+    Project: "Infra",
+    PulumiStack: pulumi.getStack()
+};
   
-// //
+//
 
-// const config = new pulumi.Config();
-// const webAddress = config.require("webAddress");
+const config = new pulumi.Config();
+const webAddress = config.require("webAddress");
 
-// const infraStack = new pulumi.StackReference("stackref-infra", {
-//   name: `/elixar-infra/${baseTags.PulumiStack}`
-// });
+const infraStack = new pulumi.StackReference("stackref-infra", {
+  name: `pernTodoApp-client/${baseTags.PulumiStack}`
+});
